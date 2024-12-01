1c87431b95cf45245c04440d70681008985a8e6a
jay <jay@mobidoc.us>
2024-12-01 16:44:43 +0900

    svelte config path 수정

--- svelte.config.js
+++ svelte.config.js
@@ -20,7 +20,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/test3' : ''
+			base: process.env.NODE_ENV === 'production' ? '/page1' : ''
 		}
 	}
 };
